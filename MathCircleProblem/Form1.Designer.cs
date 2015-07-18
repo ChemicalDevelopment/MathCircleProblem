6cd0331062938fed9d9fc02bcb6ed6bad3e5aa24
randbrown <brown.randy@gmail.com>
2015-07-18 17:20:50 -0400

    Circle math problem app

---
+++ MathCircleProblem/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace MathCircleProblem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.mathSurface = new System.Windows.Forms.Panel();
+            this.nudThetaDegrees = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudThetaRadians = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudNumberOfPoints = new System.Windows.Forms.NumericUpDown();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button7 = new System.Windows.Forms.Button();
+            this.nudSeriesIncrement = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudThetaDegrees)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudThetaRadians)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfPoints)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSeriesIncrement)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mathSurface
+            // 
+            this.mathSurface.BackColor = System.Drawing.Color.White;
+            this.mathSurface.Location = new System.Drawing.Point(183, 22);
+            this.mathSurface.Name = "mathSurface";
+            this.mathSurface.Size = new System.Drawing.Size(500, 500);
+            this.mathSurface.TabIndex = 0;
+            this.mathSurface.Paint += new System.Windows.Forms.PaintEventHandler(this.mathSurface_Paint);
+            // 
+            // nudThetaDegrees
+            // 
+            this.nudThetaDegrees.DecimalPlaces = 1;
+            this.nudThetaDegrees.Location = new System.Drawing.Point(25, 83);
+            this.nudThetaDegrees.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.nudThetaDegrees.Name = "nudThetaDegrees";
+            this.nudThetaDegrees.Size = new System.Drawing.Size(120, 20);
+            this.nudThetaDegrees.TabIndex = 1;
+            this.nudThetaDegrees.ValueChanged += new System.EventHandler(this.nudThetaDegrees_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Theta Angle (Degrees)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Theta Angle (Radians)";
+            // 
+            // nudThetaRadians
+            // 
+            this.nudThetaRadians.DecimalPlaces = 3;
+            this.nudThetaRadians.Enabled = false;
+            this.nudThetaRadians.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.nudThetaRadians.Location = new System.Drawing.Point(25, 139);
+            this.nudThetaRadians.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudThetaRadians.Name = "nudThetaRadians";
+            this.nudThetaRadians.Size = new System.Drawing.Size(120, 20);
+            this.nudThetaRadians.TabIndex = 4;
+            this.nudThetaRadians.ValueChanged += new System.EventHandler(this.nudThetaRadians_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "30";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "45";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 261);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "60";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(25, 174);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "0";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(25, 290);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "90";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(25, 22);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 10;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 383);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Number of points";
+            // 
+            // nudNumberOfPoints
+            // 
+            this.nudNumberOfPoints.DecimalPlaces = 1;
+            this.nudNumberOfPoints.Enabled = false;
+            this.nudNumberOfPoints.Location = new System.Drawing.Point(25, 399);
+            this.nudNumberOfPoints.Maximum = new decimal(new int[] {
+            1661992959,
+            1808227885,
+            5,
+            0});
+            this.nudNumberOfPoints.Name = "nudNumberOfPoints";
+            this.nudNumberOfPoints.Size = new System.Drawing.Size(120, 20);
+            this.nudNumberOfPoints.TabIndex = 11;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(25, 500);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Go Nuts";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(704, 22);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(561, 618);
+            this.chart1.TabIndex = 14;
+            this.chart1.Text = "chart1";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(25, 458);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "Chart";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // nudSeriesIncrement
+            // 
+            this.nudSeriesIncrement.DecimalPlaces = 1;
+            this.nudSeriesIncrement.Location = new System.Drawing.Point(106, 461);
+            this.nudSeriesIncrement.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.nudSeriesIncrement.Name = "nudSeriesIncrement";
+            this.nudSeriesIncrement.Size = new System.Drawing.Size(71, 20);
+            this.nudSeriesIncrement.TabIndex = 16;
+            this.nudSeriesIncrement.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1277, 652);
+            this.Controls.Add(this.nudSeriesIncrement);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudNumberOfPoints);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nudThetaRadians);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudThetaDegrees);
+            this.Controls.Add(this.mathSurface);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudThetaDegrees)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudThetaRadians)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfPoints)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSeriesIncrement)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mathSurface;
+        private System.Windows.Forms.NumericUpDown nudThetaDegrees;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudThetaRadians;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudNumberOfPoints;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.NumericUpDown nudSeriesIncrement;
+
+
+
+
+    }
+}
+
---
+++ MathCircleProblem/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MathCircleProblem
+{
+    public partial class Form1 : Form
+    {
+        private const float scale = 200; // pixels to math scale
+        private readonly float _radius;
+        private float _theta;
+        private Color _color = Color.Green;
+        private float _slop = .0001f;
+        private readonly Dictionary<decimal, int> _results = new Dictionary<decimal, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            _radius = 1 * scale;
+
+            float degrees = 45;
+            _theta = (float)(degrees * Math.PI / 180);
+          
+            nudThetaDegrees.Value = (decimal)degrees;
+            nudThetaRadians.Value = (decimal)_theta;
+
+            if (!DesignMode)
+            {
+                //SetDoubleBuffered(mathSurface);
+                // SetDoubleBuffered(this);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void mathSurface_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = mathSurface.CreateGraphics();
+
+            DrawAxesAndCircle(g);
+
+            DrawLines(g);
+        }
+
+        private PointF GetNthPoint(int n)
+        {
+            var angleToDraw = Math.PI - 2 * ((Math.PI / 2f - _theta)) * n;
+
+            float y = (float)Math.Sin(angleToDraw);
+            float x = (float)Math.Cos(angleToDraw);
+
+            return new PointF(x, y);
+        }
+
+        private List<PointF> GetPoints()
+        {
+            List<PointF> points = new List<PointF>();
+
+            //HACK arbitrary max number of points here
+            var maxPoints = 720;
+            //var maxPoints = 100;
+            for (int idx = 0; idx < maxPoints; idx++)
+            {
+                var point = GetNthPoint(idx);
+                points.Add(point);
+
+                if (idx > 0)
+                {
+                    if (Math.Abs(point.Y) < _slop && Math.Abs(point.X - (-1)) < _slop)
+                    {
+
+                        return points;
+                    }
+                }
+            }
+
+            return points;
+        }
+
+        private void DrawLines(Graphics g)
+        {
+            Pen p = new Pen(_color, 2);
+            var points = GetPoints();
+            PointF lastPoint = new PointF(-1, 0); //Start at left part of circle
+
+            for (int idx = 0; idx < points.Count; idx++)
+            {
+                var point = points[idx];
+
+                DrawMathLine(g, p, lastPoint.X, lastPoint.Y, point.X, point.Y);
+
+                lastPoint = point;
+            }
+
+            //HACK subtract 1, since the points contains our original point
+            nudNumberOfPoints.Value = points.Count - 1;
+        }
+
+        private float DegreesToRadians(float deg)
+        {
+            return (float)(deg * Math.PI / 180.0);
+        }
+
+        private float RadiansToDegrees(float rad)
+        {
+            return (float)(rad * 180.0 / Math.PI);
+        }
+
+        private void DrawMathLine(Graphics g, Pen p, float x1, float y1, float x2, float y2)
+        {
+            g.DrawLine(p, MathXtoScreenX(x1), MathYtoScreenY(y1), MathXtoScreenX(x2), MathYtoScreenY(y2));
+        }
+
+        private void DrawAxesAndCircle(Graphics g)
+        {
+            Pen myPen = new Pen(Color.Black, 2);
+
+            DrawMathLine(g, myPen, -2, (0), (2), (0));
+            DrawMathLine(g, myPen, (0), (-2), (0), (2));
+
+            var x = MathXtoScreenX(0);
+            var y = MathYtoScreenY(0);
+
+            g.DrawCircle(myPen, x, y, _radius);
+        }
+
+        public float MathXtoScreenX(float mathX)
+        {
+            float centerX = (float)(mathSurface.Width / 2.0);
+
+            return centerX + mathX * scale;
+        }
+
+        public float MathYtoScreenY(float mathY)
+        {
+            float centerY = (float)(mathSurface.Height / 2.0);
+
+            return centerY - mathY * scale;
+        }
+
+        private void nudThetaDegrees_ValueChanged(object sender, EventArgs e)
+        {
+            var deg = nudThetaDegrees.Value;
+            var rad = DegreesToRadians((float)deg);
+            nudThetaRadians.Value = (decimal)rad;
+
+            _theta = rad;
+            mathSurface.Refresh();
+        }
+
+        private void nudThetaRadians_ValueChanged(object sender, EventArgs e)
+        {
+            var rad = nudThetaRadians.Value;
+            var deg = RadiansToDegrees((float)rad);
+            nudThetaDegrees.Value = (decimal)deg;
+
+            _theta = (float)rad;
+
+            mathSurface.Refresh();
+        }
+
+        private void UpdateTheta(decimal deg)
+        {
+            nudThetaDegrees.Value = deg;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            UpdateTheta(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateTheta(30);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UpdateTheta(45);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            UpdateTheta(60);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            UpdateTheta(90);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            mathSurface.Refresh();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            bool running = timer1.Enabled;
+            if (!running)
+            {
+                _results.Clear();
+            }
+            else
+            {
+                ChartResults();
+            }
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void ChartResults()
+        {
+            chart1.Series.Clear();
+            Series ser1 = new Series("My Series", 10);
+            ser1.ChartType = SeriesChartType.Line;
+            chart1.Series.Add(ser1);
+            chart1.Series["My Series"].Points.DataBindXY(_results.Keys, _results.Values);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var angle = nudThetaDegrees.Value;
+            var max = 45;
+            angle += (max - angle) / 2;
+            _results.Clear();
+            if (Math.Abs(angle - max) > (decimal)_slop)
+            {
+                UpdateTheta(angle);
+                Application.DoEvents();
+                _results[(int)nudThetaDegrees.Value] = (int)nudNumberOfPoints.Value;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                ChartResults();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            _results.Clear();
+            ChartResults();
+
+            decimal increment = nudSeriesIncrement.Value;
+
+            for (decimal idx = 0; idx < 90; idx += increment)
+            {
+                _color = Color.FromArgb(0, (int)((idx / 90m) * 255m), (int)(((90 - idx) / 90m) * 255m));
+                UpdateTheta(idx);
+                Application.DoEvents();
+
+                _results[nudThetaDegrees.Value] = (int)nudNumberOfPoints.Value;
+
+                chart1.Series["My Series"].Points.DataBindXY(_results.Keys, _results.Values);
+
+            }
+        }
+    }
+}
